703d0e3c4728b974c42d2d22d4d636751ad4961c
abhisheksanghvi <abhisheksanghvi@172-29-16-120.DYNAPOOL.NYU.EDU>
2014-10-02 19:38:54 -0400

    QL ranker completed

--- edu/nyu/cs/cs2580/Ranker.java
+++ edu/nyu/cs/cs2580/Ranker.java
@@ -1,5 +1,7 @@
 package edu.nyu.cs.cs2580;
 
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Vector;
 import java.util.Scanner;
 
@@ -64,6 +66,48 @@ class Ranker {
         Vector<String> dv = d.get_title_vector();
 
         double score=d.get_numviews();
+        return new ScoredDocument(did, d.get_title_string(), score);
+    }
+
+    public Vector<ScoredDocument> runqueryQL(String query) {
+        Vector<ScoredDocument> retrieval_results = new Vector<ScoredDocument>();
+        for (int i = 0; i < _index.numDocs(); ++i) {
+            retrieval_results.add(runqueryQL(query, i));
+        }
+        return retrieval_results;
+    }
+
+    public ScoredDocument runqueryQL(String query, int did) {
+
+        float lambda = 0.5f;
+        Scanner s = new Scanner(query);
+        Vector<String> qv = new Vector<String>();
+        while (s.hasNext()) {
+            String term = s.next();
+            qv.add(term);
+        }
+
+        Document d = _index.getDoc(did);
+        Vector<String> dv = d.get_body_vector();
+
+        Long totalWordsInDocument = 0l;
+        HashMap<String, Integer> wordFrequency = new HashMap<String, Integer>();
+        for (String word : dv) {
+            totalWordsInDocument++;
+            if (!wordFrequency.containsKey(word)) {
+                wordFrequency.put(word, 1);
+            }
+            else {
+                wordFrequency.put(word, wordFrequency.get(word) + 1);
+            }
+        }
+
+        double score = 0.0;
+        for (String wordInQuery : dv) {
+            score = score + Math.log(((1-lambda)*(wordFrequency.get(wordInQuery)/totalWordsInDocument))
+                    +(lambda * _index.termFrequency(wordInQuery)/_index.termFrequency()));
+        }
+
         return new ScoredDocument(did, d.get_title_string(), score);
     }
 }
